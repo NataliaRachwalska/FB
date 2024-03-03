64f01794a2c039ecf4d2e7790de7d02ed15f46e5
talar <talar@LAPTOP-2JFPFR80>
2024-03-03 15:08:24 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FB
+{
+    internal class FizzBuzz
+    {
+        int upper_num = 0;
+        public FizzBuzz(int max)
+        {
+            this.upper_num = max;
+        }
+        public void display()
+        {
+            int i=1;
+            while(i <= upper_num){
+                if (i % 3 == 0)
+                {
+                    if(i%5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz Fizz = new FizzBuzz(20);
+            Fizz.display();
+        }
+    }
+}
